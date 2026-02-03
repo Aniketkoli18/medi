f095cbef18c83c772d7c1a34608a246ce7e66dba
Aniketkoli18 <159146721+Aniketkoli18@users.noreply.github.com>
2026-02-04 00:44:57 +0530

    Update DoctorServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplArraylist.java
@@ -2,36 +2,38 @@ package com.edutech.progressive.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.edutech.progressive.entity.Doctor;
 import com.edutech.progressive.service.DoctorService;
 
 public class DoctorServiceImplArraylist implements DoctorService {
-    private static List<Doctor>doctorList= new ArrayList<>();
-// public void emptyArrayList(){
-// doctorList.clear();
-// }
-public List<Doctor>getAllDoctors(){
-    return doctorList;
-}
-@Override
-public Integer addDoctor(Doctor doctor) {
-   
-    doctorList.add(doctor);
-    return doctor.getDoctorId();
-}
-@Override
-public List<Doctor> getDoctorSortedByExperience() {
-  // Collections.sort(doctorList,Comparator.comparing(Doctor::getFullName));
-  Collections.sort(doctorList);
-   return doctorList;
-}
 
-public void emptyArrayList(){
-    doctorList.clear();
-}
+    private static List<Doctor> doctorList = new ArrayList<>();
 
+    @Override
+    public List<Doctor> getAllDoctors() {
+        return doctorList;
+    }
+    @Override
+    public Integer addDoctor(Doctor doctor) {
+        doctorList.add(doctor);
+        return doctor.getDoctorId();
+
+    }
+
+    @Override
+    public List<Doctor> getDoctorSortedByExperience() {
+      Collections.sort(doctorList);
+      return doctorList;
+    }
+    @Override
+    public Doctor getDoctorById(int doctorId) throws Exception {
+        return doctorList.get(doctorId);
+    }
+    @Override
+    public void deleteDoctor(int doctorId) throws Exception {
+       deleteDoctor(doctorId);
+    }
 
 }
