7cfe1ed9af285cf1340af6f3efa9facd7b9ae562
Aniketkoli18 <159146721+Aniketkoli18@users.noreply.github.com>
2026-02-04 00:30:48 +0530

    Update DatabaseConnectionManager.java

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -8,46 +8,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
-@Component
 public class DatabaseConnectionManager {
-    private static final Properties properties=new Properties();
 
-    // @Autowired
-    // private static Environment env;
+    private static final Properties properties = new Properties();
 
-
-    private static void loadProperties(){
-     
+    private static void loadProperties() {
         try {
             properties.load(new FileInputStream("/home/coder/app/server/src/main/resources/application.properties"));
-        }
-        catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch (IOException e) {
             e.printStackTrace();
         }
-     
-
-      
-
     }
 
-    public static Connection getConnection()throws SQLException{
-        Connection connection=null;
-        try{
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
             loadProperties();
-        connection= DriverManager.getConnection(properties.getProperty("spring.datasource.url"),properties.getProperty("spring.datasource.username"),properties.getProperty("spring.datasource.password"));
-        }catch(SQLException e){
-           e.printStackTrace();
-        }
-        return connection;
-        
-        
-    }
+            connection = DriverManager.getConnection(properties.getProperty("spring.datasource.url"),
+                    properties.getProperty("spring.datasource.username"),
+                    properties.getProperty("spring.datasource.password"));
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 
 }
