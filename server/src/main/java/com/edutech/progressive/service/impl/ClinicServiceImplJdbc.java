8f87683e19da37ed343479e8eafeebd49dc2c857
Aniketkoli18 <159146721+Aniketkoli18@users.noreply.github.com>
2026-02-04 00:43:42 +0530

    Update ClinicServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJdbc.java
@@ -1,61 +1,69 @@
 package com.edutech.progressive.service.impl;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.List;
 
-import com.edutech.progressive.dao.ClinicDAOImpl;
+import com.edutech.progressive.dao.ClinicDAO;
 import com.edutech.progressive.entity.Clinic;
 import com.edutech.progressive.service.ClinicService;
 
 public class ClinicServiceImplJdbc implements ClinicService {
-    public ClinicDAOImpl clinicDAOImpl;
 
-    
 
-    public ClinicServiceImplJdbc(ClinicDAOImpl clinicDAOImpl) {
-        this.clinicDAOImpl = clinicDAOImpl;
+    private ClinicDAO clinicDAO;
+
+    public ClinicServiceImplJdbc(ClinicDAO clinicDAO) {
+        this.clinicDAO = clinicDAO;
     }
 
-    public List<Clinic>getAllClinics(){
-       try{
-        return clinicDAOImpl.getAllClinics();
-       }catch(SQLException e){
-        System.out.println(e);
-       }
-       return null;
-    }
-
-    public Clinic getClinicById(int clinicId){
-        try{
-            return clinicDAOImpl.getClinicById(clinicId);
-        }catch(Exception e){
-            System.out.println(e);
+    @Override
+    public List<Clinic> getAllClinics() throws Exception {
+       List<Clinic> clinics=null;
+        try {
+            clinics= clinicDAO.getAllClinics();
+        } catch (Exception e) {
+           throw e;
         }
-        return null;
+     
+        return clinics;
     }
 
-    public Integer addClinic(Clinic clinic){
-        try{
-            return clinicDAOImpl.addClinic(clinic);
-        }catch(Exception e){
-            System.out.println(e);
+    @Override
+    public Clinic getClinicById(int clinicId)throws Exception  {
+       try {
+             return  clinicDAO.getClinicById(clinicId);
+        } catch (Exception e) {
+           throw e;
         }
-        return null;
+      
     }
-    public void updateClinic(Clinic clinic){
-        try{
-            clinicDAOImpl.updateClinic(clinic);
-        }catch(Exception e){
-            System.out.println(e);
-        }
 
+    @Override
+    public Integer addClinic(Clinic clinic)throws Exception  {
+       try {
+             return clinicDAO.addClinic(clinic);
+        } catch (Exception e) {
+          throw e;
+        }
+      
     }
-    public void deleteClinic(int clinicId){
-        try{
-            clinicDAOImpl.deleteClinic(clinicId);
-        }catch(Exception e){
-            System.out.println(e);
+
+    @Override
+    public void updateClinic(Clinic clinic) throws Exception  {
+       try {
+            clinicDAO.updateClinic(clinic);
+        } catch (Exception e) {
+           throw e;
+        }
+       
+    }
+
+    @Override
+    public void deleteClinic(int clinicId)throws Exception  {
+         try {
+            clinicDAO.deleteClinic(clinicId);
+        } catch (Exception e) {
+          throw e;
         }
     }
 
